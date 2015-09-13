a97cba776e60f8d2c5e0c972023d3ee5faa4c3da
Bruce Webber <self@brucewebber.us>
2015-09-13 13:54:57 -0400

    Created a script to add test data to the database.

---
+++ add_testdata.py
@@ -0,0 +1,31 @@
+"""
+Inserts test data into the CREM database.
+"""
+
+from app import db
+from app.models import Track
+
+# Add tracks.
+
+track_infos = (
+    ('Literature', 'literature@penguicon.org'),
+    ('Tech', 'tech@penguicon.org'),
+    ('After Dark', 'afterdark@penguicon.org'),
+    ('Action Adventure', 'action@penguicon.org'),
+    ('Costuming', 'costuming@penguicon.org'),
+    ('Web Comics', 'webcomics@penguicon.org'),
+    ('Gaming', 'gaming@penguicon.org'),
+    ('D.I.Y.', 'diy@penguicon.org'),
+    ('Film', 'film@penguicon.org'),
+    ('Food', 'food@penguicon.org'),
+    ('Video Gaming', 'videogaming@penguicon.org'),
+    ('Science', 'science@penguicon.org'),
+    ('Eco', 'eco@penguicon.org'),
+)
+
+for track_info in track_infos:
+    track = Track(track_info[0], track_info[1])
+    db.session.add(track)
+
+# Commit the test data to the database.
+db.session.commit()
