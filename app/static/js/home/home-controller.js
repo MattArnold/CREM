1f037b74e29751eb699050d226162a7c939bae59
Matt Arnold <matt.1.arnold@gm.com>
2015-09-12 21:59:48 -0400

    Display hard-coded list of events with angular templating.

--- app/static/js/home/home-controller.js
+++ app/static/js/home/home-controller.js
@@ -1,3 +1,39 @@
 angular.module('CREM')
   .controller('HomeController', ['$scope', function ($scope) {
-  }]);
+  	// TODO: AJAXify this hard-coded data
+  	$scope.events = [
+  		{
+  			title: 'Example Title',
+  			room: 'Windover',
+  			time: '7 pm',
+  			track: 'Tech',
+  			presenters: ['Susan Simmons', 'Robert Reed'],
+  			description: 'Example of a hard-coded event. This is the description.'
+  		},
+  		{
+  			title: 'Another Example Title',
+  			room: 'Charlevoix A',
+  			time: '7 pm',
+  			track: 'Literature',
+   			presenters: ['Norman Morgenstern', 'Roselyn R. Ferguson'],
+ 			description: 'Example of another hard-coded event. This is the description of it. These are all grouped by track.'
+  		},
+  		{
+  			title: 'A Food Example Title',
+  			room: 'Charlevoix A',
+  			time: '7 pm',
+  			track: 'Food',
+   			presenters: ['Norman Morgenstern', 'Roselyn R. Ferguson'],
+ 			description: 'Example of another hard-coded event. This is the description of it.'
+  		},
+  		{
+  			title: 'A Literature Example Title',
+  			room: 'Charlevoix A',
+  			time: '7 pm',
+  			track: 'Literature',
+   			presenters: ['Norman Morgenstern', 'Roselyn R. Ferguson'],
+ 			description: 'Example of another hard-coded event. Grouped by track.'
+  		}
+   	];
+  }
+]);
