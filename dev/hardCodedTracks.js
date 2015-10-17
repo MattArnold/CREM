e2825ec84fe0a6654223e1895950fb15a0e6709a
Matt Arnold <matt.1.arnold@gm.com>
2015-10-17 16:28:34 -0400

    Organize a dev folder for code we will eventually delete

---
+++ dev/hardCodedTracks.js
@@ -0,0 +1,31 @@
+{name:'Literature',path:'literature'},{name:'Tech',path:'tech'},{name:'AfterDark',path:'afterdark'},{name:'ActionAdventure',path:'actionadventure'},{name:'Costuming',path:'costuming'},{name:'WebComics',path:'webcomics'},{name:'Gaming',path:'gaming'},{name:'D.I.Y.',path:'diy'},{name:'Film',path:'film'},{name:'Food',path:'food'},{name:'VideoGaming',path:'videogaming'},{name:'Science',path:'science'},{name:'Eco',path:'eco'},
+  	$scope.tracks = [
+  		{
+  			name: 'Literature',
+  			uid: 'literature'
+  		},
+   		{
+  			name: 'Tech',
+  			uid: 'tech'
+  		},
+  		{
+  			name: 'After Dark',
+  			uid: 'afterdark'
+  		},
+  		{
+  			name: 'Action Adventure',
+  			uid: 'actionadventure'
+  		},
+  		{
+  			name: 'Costuming',
+  			uid: 'costuming'
+  		},
+  		{
+  			name: 'Costuming',
+  			uid: 'costuming'
+  		},
+  		{
+  			name: 'Gaming',
+  			uid: 'gaming'
+  		}
+  	];
