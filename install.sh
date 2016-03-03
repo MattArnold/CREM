0432239a6eafa37baf06b6e9cb8ab5ee4713e871
Bruce Webber <self@brucewebber.us>
2016-03-02 22:19:59 -0500

    install.sh now references requirements.txt for list of Python packages.

--- install.sh
+++ install.sh
@@ -1,16 +1,4 @@
 python virtualenv.py venv
-venv/bin/pip install setuptools --no-use-wheel --upgrade
-venv/bin/pip install flask==0.9
-venv/bin/pip install flask-login
-venv/bin/pip install flask-openid
-venv/bin/pip install flask-mail==0.7.6
-venv/bin/pip install sqlalchemy==0.7.9
-venv/bin/pip install flask-sqlalchemy==0.16
-venv/bin/pip install sqlalchemy-migrate==0.7.2
-venv/bin/pip install flask-whooshalchemy==0.54a
-venv/bin/pip install flask-wtf==0.8.4
-venv/bin/pip install pytz==2013b
-venv/bin/pip install flask-babel==0.8
-venv/bin/pip install flup
+venv/bin/pip install -r requirements.txt
 bower install
 cp config.dev.py config.py
