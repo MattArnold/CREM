525389db08bc409273134ef736cdb2359706932c
Bruce Webber <self@brucewebber.us>
2016-03-02 23:16:26 -0500

    install.bat now references requirements.txt for list of Python packages.

--- install.bat
+++ install.bat
@@ -1,16 +1,4 @@
 python virtualenv.py venv
-venv\Scripts\pip install setuptools --no-use-wheel --upgrade
-venv\Scripts\pip install flask==0.9
-venv\Scripts\pip install flask-login
-venv\Scripts\pip install flask-openid
-venv\Scripts\pip install flask-mail==0.7.6
-venv\Scripts\pip install sqlalchemy==0.7.9
-venv\Scripts\pip install flask-sqlalchemy==0.16
-venv\Scripts\pip install sqlalchemy-migrate==0.7.2
-venv\Scripts\pip install flask-whooshalchemy==0.54a
-venv\Scripts\pip install flask-wtf==0.8.4
-venv\Scripts\pip install pytz==2013b
-venv\Scripts\pip install flask-babel==0.8
-venv\Scripts\pip install flup
+venv\Scripts\pip install -r requirements.txt
 bower install
 copy config.dev.py config.py
